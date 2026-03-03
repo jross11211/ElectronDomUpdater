bb6adc9baf39722843bdfb344c5c83b140c95244
jross11211 <44367789+jross11211@users.noreply.github.com>
2026-03-03 13:11:04 -0500

    Adding .env

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,16 +1,7 @@
-export const LIVE_CODESPACE_PATH = './_live_code/';
-export const LIVE_CODESPACE_SOLUTION_PATH = LIVE_CODESPACE_PATH + 'solution.py';
+import dotenv from 'dotenv';
+dotenv.config({ quiet: true });
 
-// export const URL_TARGET = 'https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/'
-export const URL_TARGET = 'https://www.google.com';
+export const URL_TARGET = process.env.URL_TARGET;
 
-export const windowConfig = {
-    show: false,
-    width: 1024,
-    height: 728,
-    webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
-        webSecurity: false,
-    },
-}
+export const LIVE_CODESPACE_PATH = process.env.LIVE_CODESPACE_PATH;
+export const LIVE_CODESPACE_SOLUTION_PATH = LIVE_CODESPACE_PATH + process.env.LIVE_CODESPACE_SOLUTION_FILE;
--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-export const func_as_string = (f: any, args: any) => {
-    let result = String(f)
-    console.log(JSON.stringify(args))
-    result = `(${result})(${JSON.stringify(args)})`
-    console.log(result)
-    return result;
-}
