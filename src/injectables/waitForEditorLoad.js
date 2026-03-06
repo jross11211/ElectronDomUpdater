bd3da6fc4665ad160e47e4c19abe913e8a1cb907
jross11211 <44367789+jross11211@users.noreply.github.com>
2026-03-05 21:17:20 -0500

    Cleaning

--- src/injectables/waitForEditorLoad.js
+++ src/injectables/waitForEditorLoad.js
@@ -2,51 +2,23 @@ export default function waitForEditorLoad(){
 
     const {ipcRenderer} = require('electron');
 
-    const testContainer = document.querySelector("[data-layout-path='/c1/ts1/t1']");
-    let last_state_str = testContainer.textContent;
-
-    const testPanelObserver = new MutationObserver(() => {
-        if (testContainer.textContent !== last_state_str){
-            const testContainerStr = testContainer.textContent;
-            console.log(testContainerStr);
-            ipcRenderer.send('tests-updated', testContainerStr);
-            last_state_str = testContainerStr;
-        }
-    });
-    testPanelObserver.observe(testContainer, {
-        childList: true,
-        subtree: true,
-        attributes: true,
-        characterData: true
-    });
-
-    let cachedEditor = null;
-    const findEditor = () => {
-
-        if (cachedEditor) {
-            return cachedEditor;
-        }
-
+    const checkIfEditorReady = () => {
         if(typeof monaco === 'undefined' || !monaco.editor){
             return null;
         }
-
         const editors = monaco.editor.getEditors();
         for (const editor of editors) {
             const model = editor.getModel();
             if (model && model.getValue().trim().length > 0) {
-                cachedEditor = model;
-                return model;
+                return editor;
             }
         }
-
         return null;
     }
 
     ipcRenderer.on('updated-solution', (_, newContent, runTests) => {
-        const model = findEditor();
         console.log('Code updated:', newContent);
-        model.setValue(newContent);
+        activeEditor.setValue(newContent);
 
         if (runTests) {
             const runBtn = document.querySelector('[data-e2e-locator="console-run-button"]');
@@ -69,13 +41,17 @@ export default function waitForEditorLoad(){
         }
     });
 
+    let activeEditor = null;
     const observer = new MutationObserver(() => {
-        if (findEditor() !== null){
 
-            console.log(findEditor().getValue());
+        activeEditor = checkIfEditorReady();
+
+        if (activeEditor){
+
+            console.log(activeEditor.getValue());
 
             const slug = window.location.pathname.split('/problems/')[1]?.replace(/\/+$/, '') ?? 'unknown';
-            ipcRenderer.send('app-fully-loaded', findEditor().getValue(), slug.split('/')[0]);
+            ipcRenderer.send('app-fully-loaded', activeEditor.getValue(), slug.split('/')[0]);
 
             observer.disconnect()
         }
